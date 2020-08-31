0aaec00154aac43e39e1b8de354af7f39bef0f6a
omnikum <omnikum@hotmail.com>
2020-08-31 16:25:37 +0300

    updating version of test

--- test_items.py
+++ test_items.py
@@ -1,5 +1,5 @@
-def test_basket_button(browser):
+def test_basket_button_is_exist(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    browser.implicitly_wait(5)
+    browser.implicitly_wait(15)
     button = len(browser.find_element_by_css_selector(".btn-add-to-basket").text)
     assert button > 0
